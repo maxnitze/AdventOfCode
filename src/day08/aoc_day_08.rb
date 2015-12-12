7d91be40125641532b45dda380d26b5526dc2657
Max Nitze <maxnitze@informatik.uni-bremen.de>
2015-12-12 19:32:11 +0100

    Add comment for source to day 8 implementation

--- src/day08/aoc_day_08.rb
+++ src/day08/aoc_day_08.rb
@@ -11,6 +11,7 @@ code_chars = 0
 real_chars = 0
 esc_chars = 0
 
+# as suggested by SnacksOnAPlane (https://www.reddit.com/r/adventofcode/comments/3vw32y/day_8_solutions/cxr8gpg)
 File.open(input_file, 'r') do |f|
   f.readlines.map(&:chomp).each do |line|
     code_chars += line.length
@@ -19,7 +20,7 @@ File.open(input_file, 'r') do |f|
   end
 end
 
-puts "The calculated total number of characters in the file is #{code_chars - real_chars}."
-puts "The calculated total number of characters in the file is #{esc_chars - code_chars}."
+puts "The calculated total number of characters in the file for part 1 is #{code_chars - real_chars}."
+puts "The calculated total number of characters in the file for part 2 is #{esc_chars - code_chars}."
 
 exit 0
