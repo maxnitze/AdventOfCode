27141073cf747a0a3f0f9638091ee3eb6e792d0a
Max Nitze <max.nitze@mgm-tp.com>
2017-12-12 01:06:45 +0100

    2017 day 02 second half

--- src/2017/day02/day_02.kts
+++ src/2017/day02/day_02.kts
@@ -1,5 +1,22 @@
 import java.io.File
 
+fun getEvenDivision(list: List<Int>, i: Int, j: Int): Int {
+    if (i >= j || i >= list.size) {
+        println("something went very wrong!")
+        System.exit(1)
+    }
+
+    return if (j >= list.size) {
+        getEvenDivision(list, i+1, i+2)
+    } else if (list[i]%list[j] == 0) {
+        list[i]/list[j]
+    } else if (list[j]%list[i] == 0) {
+        list[j]/list[i]
+    } else {
+        getEvenDivision(list, i, j+1)
+    }
+}
+
 val inputFile = File(if (!args.isEmpty()) args[0] else "")
 if (!inputFile.exists() || !inputFile.isFile()) {
     println("input file with name '${inputFile.getPath()}' does not exist or is not a file")
@@ -10,7 +27,12 @@ val inputFileMatrix = inputFile
     .readLines()
     .map { it.split("\\s+".toRegex()).map { it.toInt() } }
 
-val checksum = inputFileMatrix
+val minMaxChecksum = inputFileMatrix
     .map { it.max()!! - it.min()!! }
     .fold(0, { sum, v -> sum + v } )
-println("the checksum of the spredsheet is '$checksum'")
+println("the minimum-maximum-checksum of the spredsheet is '$minMaxChecksum'")
+
+val evenDivisionChecksum = inputFileMatrix
+    .map { getEvenDivision(it, 0, 1) }
+    .fold(0, { sum, v -> sum + v } )
+println("the even-division-checksum of the spredsheet is '$evenDivisionChecksum'")
