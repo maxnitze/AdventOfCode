8fdcc16f8046b6d247a05cf5f6fcc89908236e3b
Max Nitze <max.nitze@gmx.de>
2017-12-15 22:05:18 +0100

    2017 day 03 first half improvements

--- src/2017/day03/day_03.kts
+++ src/2017/day03/day_03.kts
@@ -2,28 +2,12 @@ import kotlin.math.absoluteValue
 import kotlin.math.ceil
 import kotlin.math.sqrt
 
-class Coordinate(val x: Int, val y: Int) {
-}
-
-fun getCoordForNumber(number: Int): Coordinate {
+fun manhattenDistanceInUlamSpiralForNumber(number: Int): Int {
     val matrixWidth = ceil(sqrt(number.toDouble())).toInt()
-
-    val outerOffset = number - (matrixWidth-1)*(matrixWidth-1)
-    val matrixSideOffset = (matrixWidth/2) - (outerOffset%matrixWidth)
-
-    // unnecessary for the solution but otherwise coordinate would be wrong
-    val coordFactor = if (matrixWidth%2 == 0) 1 else -1 
-
-    return when (outerOffset / matrixWidth) {
-        0 -> Coordinate(coordFactor*matrixWidth/2, coordFactor*(-matrixSideOffset))
-        1 -> Coordinate(coordFactor*matrixSideOffset, coordFactor*matrixWidth/2)
-        else -> throw RuntimeException("Something went very wrong here!")
-    }
+    return (matrixWidth/2).absoluteValue + ((matrixWidth/2)-((number-(matrixWidth-1)*(matrixWidth-1))%matrixWidth)).absoluteValue
 }
 
 val inputNumber = (if (!args.isEmpty()) args[0] else "325489").toInt()
 
-val coord = getCoordForNumber(inputNumber)
-val manhattenDistance = coord.x.absoluteValue + coord.y.absoluteValue
-
-println("the coordinate of input number '$inputNumber' is '(${coord.x}, ${coord.y})' with a manhatten distance of '$manhattenDistance' to the center")
+val manhattenDistance = manhattenDistanceInUlamSpiralForNumber(inputNumber)
+println("input number '$inputNumber' has a manhatten distance of '$manhattenDistance' to the center")
