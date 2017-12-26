3eb076ee15dc39e5e1339aa5de7d11cc11b864f2
Max Nitze <max.nitze@gmx.de>
2017-12-27 00:28:20 +0100

    2017 day 08 first half

---
+++ src/2017/day08/day_08.kts
@@ -0,0 +1,44 @@
+import java.io.File
+
+val inputFile = File(if (!args.isEmpty()) args[0] else "input")
+if (!inputFile.exists() || !inputFile.isFile()) {
+    println("input file with name '${inputFile.getPath()}' does not exist or is not a file")
+    System.exit(1)
+}
+
+fun compare(expr: String, registers: MutableMap<String, Int>): Boolean {
+    val (_, cmpVar, cmp, cmpVal) = """^(\S+) (==|!=|<=|>=|<|>) (\-?\d+)$""".toRegex().matchEntire(expr)!!.groups.map { it!!.value }
+    val registerVal = registers.getOrPut(cmpVar, { 0 })
+    return when (cmp) {
+        "==" -> registerVal == cmpVal.toInt()
+        "!=" -> registerVal != cmpVal.toInt()
+        "<=" -> registerVal <= cmpVal.toInt()
+        ">=" -> registerVal >= cmpVal.toInt()
+        "<"  -> registerVal <  cmpVal.toInt()
+        ">"  -> registerVal >  cmpVal.toInt()
+        else -> throw RuntimeException("something went very wrong here!")
+    }
+}
+
+val registers = mutableMapOf<String, Int>()
+val regex = """^(\S+) (inc|dec) (\-?\d+) if (\S+ (!?==|!=|<=|>=|<|>) \-?\d+)$""".toRegex()
+inputFile
+    .readLines()
+    .filter { regex.matches(it) }
+    .forEach {
+        val (_, assVar, incDec, assVal, expr) = regex.matchEntire(it)!!.groups.map { it!!.value }
+        
+        if (compare(expr, registers)) {
+            when (incDec) {
+                "inc" -> registers.put(assVar, registers.getOrPut(assVar, { 0 }) + assVal.toInt())
+                "dec" -> registers.put(assVar, registers.getOrPut(assVar, { 0 }) - assVal.toInt())
+                else -> throw RuntimeException("something went very wroong here!")
+            }
+        }
+    }
+
+val largestValue = registers
+    .map { it.value }
+    .max()
+
+println("after completing the instructions the largest value is any register is '${largestValue ?: "null"}'")
