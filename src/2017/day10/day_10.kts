c5ab5b80e7a009f0765a2c54c6629b2a9dfc6b7f
Max Nitze <max.nitze@gmx.de>
2017-12-28 18:12:21 +0100

    2017 day 10 second half

--- src/2017/day10/day_10.kts
+++ src/2017/day10/day_10.kts
@@ -39,3 +39,32 @@ val numberList = inputFile
 val firstTwoMult = numberList.stream[0]*numberList.stream[1]
 
 println("the result of multiplying the first two elements of the number list is '$firstTwoMult'")
+
+val sparseHashNumberList = (0..63)
+    .fold(NumberList(256, 0, 0), { numberList, _ ->
+        (inputFile
+            .readText()
+            .trim()
+            .toCharArray()
+            .map { it.toInt() } + listOf(17, 31, 73, 47, 23))
+            .forEach { length ->
+                (0..(length-1)/2).forEach {
+                    numberList.swap((numberList.startIdx+it)%numberList.size, (numberList.startIdx+length-1-it)%numberList.size)
+                }
+
+                numberList.incStartIndex(length)
+            }
+        
+        numberList
+    })
+
+val denseHash = sparseHashNumberList
+    .stream
+    .withIndex()
+    .groupBy { it.index / 16 }
+    .map { it.value.map { it.value } }
+    .map { it.fold(0, { r, x -> r.xor(x) }) }
+    .map { it.toString(16).padStart(2, '0') }
+    .joinToString("")
+
+println("the hexadecimal representation of the dense hash of the input is '$denseHash'")
